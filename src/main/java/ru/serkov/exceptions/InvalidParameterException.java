998cb73496c93efe660545c9f04b1c99d2920060
Egor <egor.serckov@yandex.ru>
2025-09-15 23:39:40 +0300

    add custom exceptions

---
+++ src/main/java/ru/serkov/exceptions/InvalidParameterException.java
@@ -0,0 +1,11 @@
+package ru.serkov.exceptions;
+
+public class InvalidParameterException extends RuntimeException {
+    public InvalidParameterException(String message) {
+        super(message);
+    }
+
+    public InvalidParameterException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
